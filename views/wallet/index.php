0d2fef86694b9264d1da6210054d14153d7f91bc
gitx5622 <ggitau5622@gmail.com>
2020-11-03 11:44:46 +0300

    card integration

--- views/wallet/index.php
+++ views/wallet/index.php
@@ -34,18 +34,34 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 <div class="wallet-index">
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <div class="row" style="height: 200px; border: solid; border-color: darkgrey; border-width: thin;">
+    <div class="row" style="height: 250px; border: solid; border-color: darkgrey; border-width: thin;">
         <div class="row" style="margin-top: 20px; margin-left: 20px">
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <form action="<?= Yii::$app->request->baseUrl?>/wallet/deposit">
                     <div class="input-group">
                         <span style="background-color: #3D715B; color: white;" class="input-group-addon">$</span>
-                        <input type="text" required name="amount" class="form-control" value="" placeholder="Enter amount...">
+                        <input type="number" required name="amount" class="form-control" value="" placeholder="Enter amount...">
                         <span class="input-group-btn">
                          <input  class="btn btn-default form-control" style="color: midnightblue;background-color: #D1F2EB; font-weight: 900;" type="submit" value="PayPal">
                         </span>
                     </div><!-- /input-group -->
-                    </form>
+                    </form><br/><br/><br/>
+                    <form action="<?= Yii::$app->request->baseUrl?>/wallet/deposit">
+                    <div class="input-group">
+                        <span style="background-color: #3D715B; color: white;" class="input-group-addon">$</span>
+                        <input type="number" required name="amount" class="form-control" value="" placeholder="Enter amount...">
+                        <span class="input-group-btn">
+                         <input  class="btn btn-default form-control" style="color: midnightblue;background-color: #D1F2EB; font-weight: 900;" type="submit" value="CARD">
+                        </span>
+                    </div><!-- /input-group -->
+                    </form><br/>
+                   <ul id="navigation" class="text-center">
+                    <p style="margin-bottom: -20px">Accepted payment methods</p>
+                    <li><img src="<?= Yii::$app->request->baseUrl?>/images/payment/visa.png"  width="70px" height="70px"  data-rjs="<?= Yii::$app->request->baseUrl?>/images/payment/visa.png" alt="Visa" class="pm visa"></li>
+                    <li style="margin-left: 10px"><img src="<?= Yii::$app->request->baseUrl?>/images/payment/paypal.png" width="70px" height="70px" data-rjs="<?= Yii::$app->request->baseUrl?>/images/payment/paypal.png" alt="PayPal" class="pm paypal"></li>
+                    <li style="margin-left: 10px"><img src="<?= Yii::$app->request->baseUrl?>/images/payment/american-express.png" width="60px" height="50px" data-rjs="<?= Yii::$app->request->baseUrl?>/images/payment/american-express.png" alt="MasterCard" class="pm mc"></li>
+                    <li style="margin-left: 10px"><img src="<?= Yii::$app->request->baseUrl?>/images/payment/mastercard.png" width="60px" height="60px" data-rjs="<?= Yii::$app->request->baseUrl?>/images/payment/mastercard.png" alt="MasterCard" class="pm mc"></li>
+                </ul>
                 </div><!-- /.col-lg-6 -->
         </div><!-- /.row -->
     </div>
